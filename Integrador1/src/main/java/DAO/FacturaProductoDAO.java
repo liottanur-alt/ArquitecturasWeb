e5520c312f3706ac0acc9b21ca1c259a0292d8e2
juan chantiri <juaniemilio05@gmail.com>
2026-08-29 10:36:55 -0300

    interfaz de FacturaProductoDAO

---
+++ Integrador1/src/main/java/DAO/FacturaProductoDAO.java
@@ -0,0 +1,15 @@
+package dao;
+
+import entities.FacturaProducto;
+import java.util.ArrayList;
+
+public interface FacturaProductoDAO {
+
+   
+    public void insertarDatos(ArrayList<FacturaProducto> facturasProductos);
+    
+    public void insertarDatosCsv();
+
+    public ArrayList<FacturaProducto> getFacturasProductos();
+
+}
