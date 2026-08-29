907dd7150478fb8519edd47e874a5488b421d057
Nur Liotta <liottanur@gmail.com>
2026-08-29 10:40:04 -0300

    factra entity + DAO

---
+++ Integrador1/src/main/java/DAO/FacturaDAO.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+    public interface FacturaDAO {
+
+        Factura buscarPorId(int id);
+        ArrayList<Factura> buscarTodas();
+        void insertar(Factura f);
+        void actualizar(Factura f);
+        void eliminar(Factura f);
+        void insertarDatosCsv();
+    }
+}
---
+++ Integrador1/src/main/java/entities/Factura.java
@@ -0,0 +1,9 @@
+
+import lombok.*;
+
+public class Factura {
+
+    private int idFactura;
+    private int idCliente;
+
+}
